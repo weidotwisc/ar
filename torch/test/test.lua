a74ff9ae2b06d6e611675ac67940404782ec0db2
weiz <weiz@us.ibm.com>
2017-06-12 00:14:37 -0400

    link the ar library in CMakefile of ar so that torch can use it

--- torch/test/test.lua
+++ torch/test/test.lua
@@ -1,8 +1,9 @@
 require 'ar'
+require 'cutorch'
 torch.setdefaulttensortype('torch.FloatTensor')
-x=torch.ones(1,5)
---ar.init()
---ar.allreduce(x,5)
---print(x)
---ar.finalize()
+x=torch.ones(1,5):cuda()
+ar.init()
+ar.allreduce(x,5)
+print(x)
+ar.finalize()
 
