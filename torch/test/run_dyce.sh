b594ad733944dce7c1378aceb47028302e35d4e0
weiz <weiz@us.ibm.com>
2017-06-12 13:41:37 -0400

    add run dyce.sh

---
+++ torch/test/run_dyce.sh
@@ -0,0 +1,1 @@
+mpirun --host d5,d5,d6,d6,d7,d7,d8,d8 th /mnt/nfs/nfsshare/user_homes/weiz/ar/torch/test/test.lua
--- torch/test/test.lua
+++ torch/test/test.lua
@@ -1,7 +1,7 @@
 require 'ar'
 require 'cutorch'
 torch.setdefaulttensortype('torch.FloatTensor')
---x=torch.ones(1,5):cuda()
+x=torch.ones(1,5):cuda()
 ar.init()
 ar.allreduce(x,5)
 print(x)
